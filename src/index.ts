e7c8eae65158433d0f6f802263dc384a1af9036e
remz <remi@wizito.com>
2024-10-13 20:46:13 +0200

    TypeScript typings config

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+import { Strapi } from "@strapi/strapi"
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
-};
+export default {
+  register({ strapi }: { strapi: Strapi }) {
+    // ...
+  },
+}
